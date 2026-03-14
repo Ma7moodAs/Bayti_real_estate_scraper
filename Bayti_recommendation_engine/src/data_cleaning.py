b3a4d28261ab525bcff6a3c8f5a31aaa242570c2
Ma7moodAs <ma7moodasadzarir@outlook.com>
2026-03-15 00:26:54 +0300

    Started The Data inspection phase

---
+++ Bayti_recommendation_engine/src/data_cleaning.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+#from IPython.display import display
+print('Packages imported')
+
+df = pd.read_csv(r'C:\Users\User\Desktop\Bayti_real_estate\Bayti_recommendation_engine\data\apartments.csv')
+print('Data loaded successfully')
+
+
